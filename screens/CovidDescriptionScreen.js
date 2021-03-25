c9d309111fed9f251a2b82c7e2087418dbfe6181
jlsmithjr <https://jlsmithjr@bitbucket.org/syue2/cs200spring2019team05.git>
2021-03-24 19:03:50 -0500

    Desc Screen and image

--- screens/CovidDescriptionScreen.js
+++ screens/CovidDescriptionScreen.js
@@ -6,12 +6,17 @@ import {
   Button,
   Flatlist,
   StyleSheet,
+  Image
 } from "react-native";
+
+import "../assets/CovidGerm.png";
 
 const CovidDescriptionScreen = (props) => {
   return (
     <View>
-      <Text>Covid Description Screen </Text>
+      <Image source={require('../assets/CovidGerm.png')} style={styles.image} />
+      <Text>COVID-19 is a respiratory disease caused by SARS-CoV-2, a new coronavirus discovered in 2019. The virus is thought to spread mainly from person to person through respiratory droplets produced when an infected person coughs, sneezes, or talks. Some people who are infected may not have symptoms. For people who have symptoms, illness can range from mild to severe. Adults 65 years and older and people of any age with underlying medical conditions are at higher risk for severe illness.
+</Text>
     </View>
   );
 };
@@ -19,7 +24,13 @@ const CovidDescriptionScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-  },
+  },  
+  image: {
+    //flex: 1,
+    width: '100%',
+    height: '50%',
+    //resizeMode: "center"
+  }
 });
 
 export default CovidDescriptionScreen;
