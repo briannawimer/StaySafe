49e97f74505e9af37c0fd1ff6356e7e890263014
jtadesse977 <78566451+jtadesse977@users.noreply.github.com>
2021-03-25 16:46:24 -0500

    Update CovidInfoScreen.js

--- screens/CovidInfoScreen.js
+++ screens/CovidInfoScreen.js
@@ -6,42 +6,58 @@ import {
   Button,
   Flatlist,
   StyleSheet,
+  Image,
 } from "react-native";
-
+import Card from "../components/Card";
+import "../assets/mask.jpg";
 
     const CovidInfoScreen = (props) => {
    return (
-     <View>
- 
-       <View>
-        <Text style={styles.covid}>
-
- 	        COVID INFO</Text>
-       </View>
-       <View>
+   
+        <View style={styles.screen}>
+	<Image source={require('../assets/mask.jpg')} style={styles.image} />
+	<Card style={styles.buttonContainer}>
+	<View style={styles.buttonText}>
          <Button
            title="Covid Questionnaire"
+	   color="#016A7C"
            onPress={() => {
              props.navigation.navigate({ routeName: "CovidQuestionnaire" });
            }}
          />
+	 </View>
+         </Card>
+	<Card style={styles.buttonContainer}>
+        <View>
          <Button
            title="Covid Map"
+	   color="#016A7C"
            onPress={() => {
              props.navigation.navigate({ routeName: "CovidCaseMap" });
            }}
          />
+ 	</View>
+      	</Card>
+	<Card style={styles.buttonContainer}>
+        <View style={styles.buttonText}>
          <Button
            title="Covid Description"
+	   color="#016A7C"
            onPress={() => {
              props.navigation.navigate({ routeName: "CovidDescription" });
            }}
          />
-       </View>
+	</View>
+      </Card>
+ 
      </View>
    );
  };
+
 const styles = StyleSheet.create({
+  screen: {
+	flex: 1, 
+   },
   covid: {
      color: '#32425F',
      fontSize: 40,
@@ -66,10 +82,29 @@ const styles = StyleSheet.create({
      borderRadius: 5,
    },
 
-   buttonText: {
-     fontSize: 20,
-     color: '#000000',
-   },
+   buttonContainer: {
+    flexDirection: "row",
+    margin: 12,
+    padding: 10,
+    justifyContent: "center",
+    textAlign: "center",
+    paddingHorizontal: 100,
+  },
+
+  buttonText: {
+    width: "100%",
+    
+  },
+
+  image: {
+    //flex: 1,
+    width: '100%',
+    height: '51%',
+    alignSelf: "center",
+    //marginVertical: 20,
+    
+    //resizeMode: "center"
+  },
 });
 
 
