5a1ca4e0382a06bc91ff7764170cf9de092cd5d7
jtadesse977 <78566451+jtadesse977@users.noreply.github.com>
2021-03-24 21:28:11 -0500

    Update VaccineSchedulerScreen.js

--- screens/VaccineSchedulerScreen.js
+++ screens/VaccineSchedulerScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,19 +6,64 @@ import {
   Button,
   Flatlist,
   StyleSheet,
+  SafeAreaView,
 } from "react-native";
 
 const VaccineSchedulerScreen = (props) => {
   return (
     <View>
-      <Text>Vaccine Scheduler Screen </Text>
+     <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Choose a date to be reminded to take your vaccine second dose
+        </Text>
+        <DatePicker
+          style={styles.datePickerStyle}
+          date={date} //initial date from state
+          mode="date" //The enum of date, datetime and time
+          placeholder="select date"
+          format="MM-DD-YYYY"
+          minDate="03-01-2021"
+          maxDate="01-01-2022"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              //display: 'none',
+              position: 'absolute',
+              left: 0,
+              top: 2,
+              marginLeft: 0,
+            },
+            dateInput: {
+              marginLeft: 39,
+            },
+          }}
+          onDateChange={(date) => {
+            setDate(date);
+          }}
+        />
     </View>
+</SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
     flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  datePickerStyle: {
+    width: 200,
+    marginTop: 20,
   },
 });
 
