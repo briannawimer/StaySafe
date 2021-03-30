f8857165d18f3a3af9f221446ef1dbcc4f5e88c4
blwimer <blwimer@crimson.ua.edu>
2021-03-30 09:56:18 -0500

    COM

--- data/questionnaire-data.js
+++ data/questionnaire-data.js
@@ -11,3 +11,11 @@ export const SYMPTOMS = [
   new Symptoms("s8", "Diarrhea"),
   new Symptoms("s9", "Headache"),
 ];
+
+export const CATEGORIES = [
+  new Symptoms("c1", "HealthCare Worker/First Provider"),
+  new Symptoms("c2", "Age >= 55"),
+  new Symptoms("c3", "Age between 16 to 64 with high-risk medical conditions"),
+  new Symptoms("c4", "Have an intellectual or developmental disorder"),
+  new Symptoms("c5", "Other nonessential critical workers"),
+];
--- screens/VaccineQuestionaireScreen.js
+++ screens/VaccineQuestionaireScreen.js
@@ -4,14 +4,41 @@ import {
   Text,
   TouchableOpacity,
   Button,
-  Flatlist,
+  FlatList,
   StyleSheet,
 } from "react-native";
-
+import Card from "../components/Card";
+import { CATEGORIES } from "../data/questionnaire-data";
 const VaccineQuestionnaireScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity style={styles.gridItem}>
+        <Card>
+          <View>
+            <Text>{itemData.item.title}</Text>
+          </View>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View>
-      <Text>Vaccine Questionnaire Screen </Text>
+      <Text style={styles.instructions}>
+        Click if you fit any of these categories
+      </Text>
+      <View>
+        <FlatList
+          data={CATEGORIES}
+          renderItem={renderGridItem}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+        />
+      </View>
+      <View style={styles.screen}>
+        <TouchableOpacity style={styles.submitBtn}>
+          <Text style={styles.submitText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -19,6 +46,33 @@ const VaccineQuestionnaireScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    marginTop: 10,
+    padding: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  instructions: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 40,
+    fontSize: 24,
+  },
+  gridItem: {
+    flex: 1,
+    margin: 5,
+    height: 65,
+  },
+  submitBtn: {
+    width: "30%",
+    borderRadius: 15,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "blue",
+    marginBottom: 10,
+  },
+  submitText: {
+    color: "white",
   },
 });
 
