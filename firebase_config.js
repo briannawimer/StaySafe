50b1b94a153ab7fa7adbb55c4859567ba4672731
pengpeng234-cloud <76507161+pengpeng234-cloud@users.noreply.github.com>
2021-04-11 22:58:39 -0500

    database set
    yarn add database

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+
+import * as firebase from 'firebase';
+
+
+
+config firebaseConfig = {
+    apiKey: "AIzaSyAVwTL841_qf_rLfIbdGBhO7XRb6Yn8mgY",
+    authDomain: "staysafe-1c3e3.firebaseapp.com",
+    projectId: "staysafe-1c3e3",
+    storageBucket: "staysafe-1c3e3.appspot.com",
+    messagingSenderId: "63594779330",
+    appId: "1:63594779330:web:4d40e61bfb8f7b7989a15b",
+    measurementId: "G-BTMSYL9XX0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
